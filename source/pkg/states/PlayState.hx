6cf23c7625ef466772578d8c12063b2edec2fdf2
wyrda <wyrda@protonmail.com>
2022-10-01 15:11:44 -0400

    Tabula rasa

---
+++ source/pkg/states/PlayState.hx
@@ -0,0 +1,13 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	override public function create() {}
+
+	override public function update(elapsed:Float) {}
+
+	function enemydies(objA:FlxSprite, objB:FlxSprite):Void {}
+}
---
+++ source/pkg/states/StartState.hx
@@ -0,0 +1,19 @@
+package pkg.states;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+
+class StartState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
